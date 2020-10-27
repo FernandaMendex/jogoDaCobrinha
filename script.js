ebe24cd99a0e6f1f0dcba066f99ddfaee2900bc0
FernandaMendex <fernandamendesq@hotmail.com>
2020-10-26 23:22:15 -0300

    Ajuste de aparência

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "black";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
     for(i=0; i < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "white";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function comidinha() {
-    context.fillStyle = "red";
+    context.fillStyle = "green";
     context.fillRect(food.x, food.y, box, box);
 }
 
